e13c34044820543c90f37b5793c20dc5e300c204
khactuan1710 <khactuan1710@gmail.com>
2023-02-02 01:08:11 +0700

    up

---
+++ App.js
@@ -0,0 +1,92 @@
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Image, TextInput, TouchableOpacity, Alert} from 'react-native';
+
+export default function App() {
+  const [listCustomer, setListCustomer] = useState();
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const getListCustomer = async() => {
+     const res = await fetch('https://60f4d20e2208920017f39df5.mockapi.io/customer')
+      .then(response => response.json())
+      .then(json => {
+        setListCustomer(json)
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+
+  useEffect( () => {
+    getListCustomer();
+   
+  }, [])
+  const renderPost = (item) => {
+    return (<View>
+      <Text>{item.name}</Text>
+      <Image  style={{width: 200, height: 200}}
+        source={{
+          uri: item.avatar
+        }}/>
+    </View>)
+  }
+
+  const validate = () => {
+    var isCorrect = false
+    listCustomer.forEach(element => {
+      if(username == element.username && password == element.password) {
+        isCorrect = true;
+      }
+    });
+    
+    if(isCorrect) {
+      Alert.alert("dang nhap thanh cong")
+    }else {
+      Alert.alert("dang nhap that bai")
+    }
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={{marginTop: 40}}>Danh Sach Bai Viet</Text>
+      <TextInput
+        style={{width: 300, height: 40, borderRadius: 20, borderColor: '#c4c4c4', borderWidth: 1, paddingLeft: 8, marginTop: 20}}
+        onChangeText={(text) => {
+          setUsername(text)
+          console.log(username, "username");
+        }}
+        placeholder="input username"
+      />
+      <TextInput
+        style={{width: 300, height: 40, borderRadius: 20, borderColor: '#c4c4c4', borderWidth: 1, paddingLeft: 8, marginTop: 20}}
+        onChangeText={(text) => {
+          setPassword(text)
+          console.log(password, "password");
+        }}
+        placeholder="input password"
+        secureTextEntry={true}
+        keyboardType=""
+      />
+      <TouchableOpacity
+      onPress={validate}
+       style={{marginTop: 30, width: 300, height: 40, borderRadius: 20, alignContent: 'center', justifyContent: "center", backgroundColor: '#ee0033'}} >
+        <Text style={{color: "#ffffff", alignSelf: 'center'}}>Login</Text>
+      </TouchableOpacity>
+      {/* <FlatList
+        data={listCustomer}
+        renderItem={({item}) => renderPost(item)}
+        keyExtractor={item => item.id}
+      /> */}
+  
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+});
